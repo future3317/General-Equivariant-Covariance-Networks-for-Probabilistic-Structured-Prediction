f571417c6e3c2332b23c9c9bc0abc9c89abfdeda
future 3317 <2802300109@qq.com>
2026-07-23 09:39:21 +0800

    suppress upstream PyG deprecations at compatibility boundary

--- compatibility/torch_geometric.py
+++ compatibility/torch_geometric.py
@@ -16,11 +16,27 @@ _TORCHSCRIPT_DEPRECATION = (
     r"`torch\.jit\.script` is deprecated\. "
     r"Please switch to `torch\.compile` or `torch\.export`\."
 )
+_PYG_DISTRIBUTED_DEPRECATION = (
+    r"`torch_geometric\.distributed` has been deprecated since 2\.7\.0.*"
+)
+_PYG_TORCH_CLUSTER_DEPRECATION = (
+    r"'torch-cluster' is no longer necessary and is being ignored\..*"
+)
 
 with warnings.catch_warnings():
     warnings.filterwarnings(
         "ignore",
         message=_TORCHSCRIPT_DEPRECATION,
+        category=DeprecationWarning,
+    )
+    warnings.filterwarnings(
+        "ignore",
+        message=_PYG_DISTRIBUTED_DEPRECATION,
+        category=DeprecationWarning,
+    )
+    warnings.filterwarnings(
+        "ignore",
+        message=_PYG_TORCH_CLUSTER_DEPRECATION,
         category=DeprecationWarning,
     )
     from torch_geometric.data import Data
